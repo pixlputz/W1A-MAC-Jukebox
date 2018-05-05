5a0a732a19bb0aec51a1d2f21773c20eabdddb04
pixlputz <pixlputz@gmail.com>
2018-05-05 11:24:25 -0600

    2018-05-05-1123: Update

---
+++ W1A-MAC-Jukebox/Classes/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace W1A_MAC_Jukebox.Classes
+{
+   class Note
+   {
+
+   }
+
+}
---
+++ W1A-MAC-Jukebox/Classes/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace W1A_MAC_Jukebox.Classes
+{
+   class Song
+   {
+
+
+   }
+
+}
