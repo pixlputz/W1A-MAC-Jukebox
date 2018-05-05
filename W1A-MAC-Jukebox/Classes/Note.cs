acc9614c0f38cadceff9261cbe17c55b7a52da8b
pixlputz <pixlputz@gmail.com>
2018-05-05 12:31:37 -0600

    2018-05-05-1231: Note Class Built

--- W1A-MAC-Jukebox/Classes/Note.cs
+++ W1A-MAC-Jukebox/Classes/Note.cs
@@ -5,12 +5,20 @@ using System.Text;
 
 namespace W1A_MAC_Jukebox.Classes
 {
-   //* AWA: Will be used to store Frequency and Duration.
-   //* AWA: the Song will use these properties to populate arguments required for the Console.Beep() command.
-   
+   //* CW: Will be used to store Frequency and Duration.
+   //* CW: The Song will use these properties to populate arguments required for the Console.Beep() command.
+   //* CW: Build Constructor to populate required Frequency and Duration properties.
+
    class Note
    {
+      public int Frequency { get; set; }
+      public int duration { get; set; }
 
+      public Note(int f, int d)
+      {
+         Frequency = f;
+         duration = d;
+      }
 
    }
 
