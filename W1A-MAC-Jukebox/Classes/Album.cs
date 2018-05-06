781e04d61e5817883e1d637bb2ffe3e1ce3938c8
pixlputz <pixlputz@gmail.com>
2018-05-06 10:21:14 -0600

    2018-05-06-1021: BOD

--- W1A-MAC-Jukebox/Classes/Album.cs
+++ W1A-MAC-Jukebox/Classes/Album.cs
@@ -7,7 +7,16 @@ namespace W1A_MAC_Jukebox.Classes
 {
    class Album
    {
+      public int ID { get; set; }
+      public string Description { get; set; }
 
+      public List<Song> Songs = new List<Song>();
+
+      public Album(int id, string description)
+      {
+         ID = id;
+         Description = description;
+      }
 
    }
 
--- W1A-MAC-Jukebox/Classes/Song.cs
+++ W1A-MAC-Jukebox/Classes/Song.cs
@@ -5,16 +5,40 @@ using System.Text;
 
 namespace W1A_MAC_Jukebox.Classes
 {
-   //* AWA: Contain unique identifier of some kind (name/ID), used by Jukebox to identify & select desired Song object.
-   //* AWA: Contain list of Note objects: Properties: Times played, Description, etc - details to display under selection process.
-   //* AWA: Play() method: Allow user to play each Note comprising the Song.
-   //* AWA: Use Console.Beep() method: Accecpting a frequency, and duration for each Note.
+   //* CW: Contain unique identifier of some kind (name/ID), used by Jukebox to identify & select desired Song object.
+   //* CW: Properties: Times played, Description, etc - details to display under selection process.
+   //* CW: Contain list of Note objects.
+   //* CW: Play() method: Allow user to play each Note comprising the Song.
+   //* CW: Use Console.Beep() method: Accecpting a frequency, and duration for each Note.
    //       - Playing a single note looks something like Console.Beep(1080, 5000).
    //       - Our Play() method will utilize this built-in method to play each note.
 
    class Song
    {
+      public int ID { get; set; }
+      public string Description { get; set; }
+      public int TimesPlayed { get; set; }
 
+      public List<Note> Notes = new List<Note>();
+
+      public Song(int id, string description)
+      {
+         ID = id;
+         Description = description;
+         TimesPlayed = 0;
+      }
+
+      public void Play()
+      {
+         //AWA: Build method content...
+         //...
+         foreach (Note n in Notes)
+         {
+            Console.Beep(n.Frequency, n.duration);
+         }
+         //Increment TimesPlayed:
+         this.TimesPlayed++;
+      }
 
    }
 
